5174d56f7b3d03fb91255e00acbc45ceb2f1b037
misdarmanto <misdarmanto1922@gmail.com>
2024-04-08 14:43:16 +0700

    _

--- apps/configs/index.js
+++ apps/configs/index.js
@@ -23,11 +23,11 @@ exports.CONFIG = {
     pinEncryption: 'qwerty',
     token: 'qwerty'
   },
-  // twilio: {
-  //     accountSID: process.env.TWILIO_ACCOUNT_SID,
-  //     authToken: process.env.TWILIO_AUTH_TOKEN,
-  //     verifyService: process.env.TWILIO_VERIFY_SERVICE
-  // },
+  twilio: {
+    accountSID: process.env.TWILIO_ACCOUNT_SID,
+    authToken: process.env.TWILIO_AUTH_TOKEN,
+    verifyService: process.env.TWILIO_VERIFY_SERVICE
+  },
   maximumUploadFile: 1024,
   dataBase: {
     development: {
@@ -56,51 +56,3 @@ exports.CONFIG = {
     }
   }
 }
-// export const CONFIG = {
-//   appVersion: process.env.APP_VERSION,
-//   appSemantic: process.env.APP_SEMANTIC,
-//   appMode: process.env.APP_MODE ?? 'development',
-//   appUrl: process.env.APP_URL,
-//   env: process.env.APP_ENV,
-//   port: process.env.APP_PORT ?? 8000,
-//   log: process.env.APP_LOG === 'true',
-//   ipBlackList: JSON.parse(process.env.IP_BLACK_LIST ?? '[]'),
-//   secret: {
-//     keyEncryption: process.env.SECRET_KEY_ENCRYPTION,
-//     passwordEncryption: process.env.SECRET_PASSWORD_ENCRYPTION,
-//     pinEncryption: process.env.SECRET_PIN_ENCRYPTION,
-//     token: process.env.TOKEN_SECRET
-//   },
-//   maximumUploadFile: process.env.MAXIMUM_UPLOAD_FILE ?? 1024,
-//   twilio: {
-//     accountSID: process.env.TWILIO_ACCOUNT_SID,
-//     authToken: process.env.TWILIO_AUTH_TOKEN,
-//     verifyService: process.env.TWILIO_VERIFY_SERVICE
-//   },
-//   dataBase: {
-//     development: {
-//       username: process.env.DB_USER_NAME,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_NAME,
-//       host: process.env.DB_HOST,
-//       dialect: process.env.DB_DIALECT,
-//       logging: process.env.DB_LOG === 'true'
-//     },
-//     testing: {
-//       username: process.env.DB_USER_NAME,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_NAME,
-//       host: process.env.DB_HOST,
-//       dialect: process.env.DB_DIALECT,
-//       logging: process.env.DB_LOG === 'true'
-//     },
-//     production: {
-//       username: process.env.DB_USER_NAME,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_NAME,
-//       host: process.env.DB_HOST,
-//       dialect: process.env.DB_DIALECT,
-//       logging: process.env.DB_LOG === 'true'
-//     }
-//   }
-// }
